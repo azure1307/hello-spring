342277604d372c08647a6c654f6e0d879632d298
azure1307 <azure1307@gmail.com>
2025-03-18 16:57:43 +0900

    0318 7일차 DB 접근 기술, AOP (끝)

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,7 +25,7 @@ public class MemberService {
     public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
-       return member.getId();
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -37,7 +39,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-       return memberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
